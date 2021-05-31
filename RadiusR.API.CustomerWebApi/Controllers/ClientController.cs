8eff90b67c8314e7e767cdab3c732eff5ba88804
Onur Civanoğlu <onurcivanoglu@yandex.com>
2021-05-31 17:51:18 +0300

    fixed minor issues

--- RadiusR.API.CustomerWebApi/Controllers/ClientController.cs
+++ RadiusR.API.CustomerWebApi/Controllers/ClientController.cs
@@ -46,8 +46,8 @@ namespace RadiusR.API.CustomerWebApi.Controllers
     {
         WebServiceLogger Errorslogger = new WebServiceLogger("Errors");
         WebServiceLogger CustomerInComingInfo = new WebServiceLogger("CustomerInComingInfo");
+        WebServiceLogger MobileLogger = new WebServiceLogger("MobileAppLog");
         readonly RadiusR.Address.AddressManager AddressClient = new RadiusR.Address.AddressManager();
-
         [HttpPost]
         public CustomerServiceActivateAutomaticPaymentResponse ActivateAutomaticPayment(CustomerServiceActivateAutomaticPaymentRequest request)
         {
@@ -4830,6 +4830,178 @@ namespace RadiusR.API.CustomerWebApi.Controllers
                 {
                     ResponseMessage = CommonResponse.InternalException(request.Culture),
                     SubscriptionList = null,
+                };
+            }
+        }
+        [HttpPost]
+        public CustomerServiceAppLogResponse AppLog(CustomerServiceAppLogRequest request)
+        {
+            var password = new ServiceSettings().GetUserPassword(request.Username);
+            var passwordHash = HashUtilities.GetHexString<SHA1>(password);
+            try
+            {
+                CustomerInComingInfo.LogIncomingMessage(request);
+                if (!request.HasValidHash(passwordHash, Properties.Settings.Default.CacheDuration))
+                {
+                    Errorslogger.LogException(request.Username, new Exception("unauthorize error"));
+                    return new CustomerServiceAppLogResponse(passwordHash, request)
+                    {
+                        AppLogResult = false,
+                        ResponseMessage = CommonResponse.UnauthorizedResponse(request)
+                    };
+                }
+                if (!string.IsNullOrEmpty(request.LogDescription))
+                {
+                    MobileLogger.LogException(request.Username, new Exception(request.LogDescription));
+                }
+                return new CustomerServiceAppLogResponse(passwordHash, request)
+                {
+                    AppLogResult = true,
+                    ResponseMessage = CommonResponse.SuccessResponse(request.Culture)
+                };
+            }
+            catch (Exception ex)
+            {
+                Errorslogger.LogException(request.Username, ex);
+                //Errorslogger.Error(ex, "Error Get new customer register");
+                return new CustomerServiceAppLogResponse(passwordHash, request)
+                {
+                    ResponseMessage = CommonResponse.InternalException(request.Culture),
+                    AppLogResult = false
+                };
+            }
+        }
+        [HttpPost]
+        public CustomerServiceExistingCustomerRegisterResponse ExistingCustomerRegister(CustomerServiceExistingCustomerRegisterRequest request)
+        {
+            var password = new ServiceSettings().GetUserPassword(request.Username);
+            var passwordHash = HashUtilities.GetHexString<SHA1>(password);
+            try
+            {
+                CustomerInComingInfo.LogIncomingMessage(request);
+                if (!request.HasValidHash(passwordHash, Properties.Settings.Default.CacheDuration))
+                {
+                    Errorslogger.LogException(request.Username, new Exception("unauthorize error"));
+                    return new CustomerServiceExistingCustomerRegisterResponse(passwordHash, request)
+                    {
+                        KeyValuePairs = null,
+                        ResponseMessage = CommonResponse.UnauthorizedResponse(request)
+                    };
+                }
+                using (var db = new RadiusR.DB.RadiusREntities())
+                {
+                    var referenceCustomer = db.Subscriptions.Find(request.ExistingCustomerRegister.SubscriberID);
+                    if (referenceCustomer == null)
+                    {
+                        return new CustomerServiceExistingCustomerRegisterResponse(passwordHash, request)
+                        {
+                            ResponseMessage = CommonResponse.SubscriberNotFoundErrorResponse(request.Culture),
+                            KeyValuePairs = null
+                        };
+                    }
+                    var specialOfferId = db.SpecialOffers.Where(s => s.IsReferral == true && DateTime.Now > s.StartDate && DateTime.Now < s.EndDate).ToList();
+                    if (request.ExistingCustomerRegister.RegistrationInfo.ReferralDiscount != null)
+                    {
+                        if (specialOfferId.Count != 1)
+                        {
+                            return new CustomerServiceExistingCustomerRegisterResponse(passwordHash, request)
+                            {
+                                KeyValuePairs = null,
+                                ResponseMessage = CommonResponse.SpecialOfferError(request.Culture)
+                            };
+                        }
+                    }
+                    var externalTariff = db.ExternalTariffs.GetActiveExternalTariffs().FirstOrDefault(ext => ext.TariffID == request.ExistingCustomerRegister.RegistrationInfo.ServiceID);
+                    if (externalTariff == null)
+                    {
+                        return new CustomerServiceExistingCustomerRegisterResponse(passwordHash, request)
+                        {
+                            ResponseMessage = CommonResponse.TariffNotFound(request.Culture),
+                            KeyValuePairs = null
+                        };
+                    }
+                    var billingPeriod = externalTariff.Service.GetBestBillingPeriod(DateTime.Now.Day);
+                    var AddressInfo = request.ExistingCustomerRegister.RegistrationInfo.SetupAddress;
+                    var ReferralDiscount = request.ExistingCustomerRegister.RegistrationInfo.ReferralDiscount;
+                    var registrationInfo = new CustomerRegistrationInfo.SubscriptionRegistrationInfo()
+                    {
+                        RegistrationType = (SubscriptionRegistrationType)request.ExistingCustomerRegister.ExtraInfo.ApplicationType,
+                        TransitionPSTN = request.ExistingCustomerRegister.ExtraInfo.PSTN,
+                        TransitionXDSLNo = request.ExistingCustomerRegister.ExtraInfo.XDSLNo,
+                        GroupIds = RadiusR.DB.CustomerWebsiteSettings.CustomerWebsiteRegistrationGroupID == null ? null : new int[] { RadiusR.DB.CustomerWebsiteSettings.CustomerWebsiteRegistrationGroupID.Value },
+                        BillingPeriod = billingPeriod,
+                        CommitmentInfo = null,
+                        DomainID = externalTariff.DomainID,
+                        ServiceID = externalTariff.TariffID,
+                        SetupAddress = new CustomerRegistrationInfo.AddressInfo()
+                        {
+                            AddressNo = AddressInfo.AddressNo,
+                            AddressText = AddressInfo.AddressText,
+                            ApartmentID = AddressInfo.ApartmentID,
+                            ApartmentNo = AddressInfo.ApartmentNo,
+                            DistrictID = AddressInfo.DistrictID,
+                            DistrictName = AddressInfo.DistrictName,
+                            DoorID = AddressInfo.DoorID,
+                            DoorNo = AddressInfo.DoorNo,
+                            Floor = AddressInfo.Floor,
+                            NeighbourhoodID = AddressInfo.NeighbourhoodID,
+                            NeighbourhoodName = AddressInfo.NeighbourhoodName,
+                            PostalCode = AddressInfo.PostalCode,
+                            ProvinceID = AddressInfo.ProvinceID,
+                            ProvinceName = AddressInfo.ProvinceName,
+                            RuralCode = AddressInfo.RuralCode,
+                            StreetID = AddressInfo.StreetID,
+                            StreetName = AddressInfo.StreetName,
+                        },
+                        ReferralDiscount = ReferralDiscount == null ? null : new CustomerRegistrationInfo.ReferralDiscountInfo()
+                        {
+                            ReferenceNo = ReferralDiscount.ReferenceNo,
+                            SpecialOfferID = request.ExistingCustomerRegister.RegistrationInfo.ReferralDiscount != null ? specialOfferId.FirstOrDefault().ID : null
+                        }
+                    };
+                    var result = RadiusR.DB.Utilities.ComplexOperations.Subscriptions.Registration.Registration.RegisterSubscriptionForExistingCustomer(db, registrationInfo, referenceCustomer.Customer);
+                    Dictionary<string, string> valuePairs = new Dictionary<string, string>();
+                    if (!result.IsSuccess)
+                    {
+                        var dic = result.ValidationMessages.ToDictionary(x => x.Key, x => x.ToArray());
+                        foreach (var item in dic)
+                        {
+                            valuePairs.Add(item.Key, string.Join("-", item.Value.Select(m => !string.IsNullOrEmpty(m))));
+                        }
+                        return new CustomerServiceExistingCustomerRegisterResponse(passwordHash, request)
+                        {
+                            ResponseMessage = CommonResponse.FailedResponse(request.Culture),
+                            KeyValuePairs = valuePairs
+                        };
+                    }
+                    db.SaveChanges();
+                    db.SystemLogs.Add(RadiusR.SystemLogs.SystemLogProcessor.AddSubscription(null, referenceCustomer.ID, referenceCustomer.Customer.ID, (SubscriptionRegistrationType)request.ExistingCustomerRegister.ExtraInfo.ApplicationType, SystemLogInterface.CustomerWebsite, $"{request.Username} ({referenceCustomer.SubscriberNo})", referenceCustomer.SubscriberNo));
+                    db.SaveChanges();
+                    return new CustomerServiceExistingCustomerRegisterResponse(passwordHash, request)
+                    {
+                        KeyValuePairs = valuePairs,
+                        ResponseMessage = CommonResponse.SuccessResponse(request.Culture)
+                    };
+                }
+            }
+            catch (NullReferenceException ex)
+            {
+                Errorslogger.LogException(request.Username, ex);
+                //Errorslogger.Error(ex, "Error Null Reference Exception");
+                return new CustomerServiceExistingCustomerRegisterResponse(passwordHash, request)
+                {
+                    ResponseMessage = CommonResponse.NullObjectException(request.Culture),
+                    KeyValuePairs = null
+                };
+            }
+            catch (Exception ex)
+            {
+                Errorslogger.LogException(request.Username, ex);
+                //Errorslogger.Error(ex, "Error Get new customer register");
+                return new CustomerServiceExistingCustomerRegisterResponse(passwordHash, request)
+                {
+                    ResponseMessage = CommonResponse.InternalException(request.Culture),
+                    KeyValuePairs = null
                 };
             }
         }
